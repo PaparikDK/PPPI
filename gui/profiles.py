fb7763e44b74e1fbb92a89e75ad023d2e0db151e
Danil Kravchuk <98008821+PaparikDK@users.noreply.github.com>
2025-02-18 14:26:14 +0300

    Update profiles.py

--- gui/profiles.py
+++ gui/profiles.py
@@ -7,12 +7,14 @@ class Order:
         self.payment = payment
 
 def generate_order():
-    cargo_types = ['фрукты', 'стройматериалы', 'техника', 'нефть']
-    distance = random.randint(50, 1000)                                 # расстояние от 50 до 1000 км
+    cargo_types = ['фрукты', 'стройматериалы', 'цемент', 'техника', 'нефть']
+    distance = random.randint(50, 1200)                                 # расстояние от 50 до 1200 км
     payment = distance * random.uniform(0.5, 2.0)                       # оплата зависит от расстояния
     cargo_type = random.choice(cargo_types)
     
     return Order(cargo_type, distance, payment)
 
 new_order = generate_order()
+new_orded_temp = generate_order()
+
 print(f"Заказ: {new_order.cargo_type}, Расстояние: {new_order.distance} км, Оплата: {new_order.payment:.2f} руб.")
